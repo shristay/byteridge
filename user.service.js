89f48d5caa40cc05560045b3d12fb107f60931f8
shristay <68692219+shristay@users.noreply.github.com>
2022-07-26 18:30:46 +0530

    Add files via upload

---
+++ user.service.js
@@ -0,0 +1,101 @@
+﻿const config = require('config.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+
+module.exports = {
+    authenticate,
+    authenticate1,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function authenticate({ username, password }) {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+}
+
+
+async function getAll() {
+    return await User.find().select('-hash');
+}
+
+async function getById(id) {
+    return await User.findById(id).select('-hash');
+}
+
+async function create(userParam) {
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
+
+async function update(id, userParam) {
+    const user = await User.findById(id);
+
+    // validate
+    if (!user) throw 'User not found';
+    if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // copy userParam properties to user
+    Object.assign(user, userParam);
+
+    await user.save();
+}
+
+async function _delete(id) {
+    await User.findByIdAndRemove(id);
+}
+
+
+//User can login and logout as a normal user.
+//Change the login API to also record the
+// login/logout of a user with the timestamp and the client IP
+// SUB-TASK 2. Implement a new API (GET /audit) that will
+// retrieve all login/logout of users with the timestamp and the
+// client IP
+// a. If a user is not having the AUDITOR role the /audit API
+// should return an HTTP 401 response
+
+async function authenticate1({ username, password }) {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        user.clientIp = '172.80.67.12'
+        await user.save();
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+}
---
+++ users.controller.js
@@ -0,0 +1,75 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+const db = require('_helpers/db');
+const User = db.User;
+
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+// Implement a new API (GET /audit) that will
+// retrieve all login/logout of users with the timestamp and the
+// client IP
+
+router.get('/audit', async(req,res)=>{
+  const user = await User.findOne({ username });
+    if (user.role !== "Auditor"){
+        res.status(401).send("User don't have the access");
+    }else{
+        const data = await User.find({});
+        res.json(data)
+    }
+
+  //res.json(data);
+
+})
+
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate1(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(err => next(err));
+}
+
+function getCurrent(req, res, next) {
+    userService.getById(req.user.sub)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
